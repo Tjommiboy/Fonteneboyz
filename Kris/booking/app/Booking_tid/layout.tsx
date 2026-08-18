06f9ab9fb67c78af4be63e518b5454449022bce7
Schwi_Shiro <kristoffer_jakobsen@hotmail.com>
2026-08-18 14:45:09 +0200

    la til metadata

--- Kris/booking/app/Booking_tid/layout.tsx
+++ Kris/booking/app/Booking_tid/layout.tsx
@@ -1,4 +1,10 @@
 import { ReactNode } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Booking Dato og Tid',
+    description: 'Her legger vi inn dato og tid for bookingen'
+}
 
 export default function BookingPage({children}: {children: ReactNode}){
     return (
