ef6a753d6541705d608a2b208f69d22b9dc6f26b
Schwi_Shiro <kristoffer_jakobsen@hotmail.com>
2026-08-21 00:11:54 +0200

    lagt til tekst i app/page.tsx

--- Kris/booking/app/page.tsx
+++ Kris/booking/app/page.tsx
@@ -5,12 +5,16 @@ export default function Home() {
   return (
       <main className="flex justify-center">
         <div className=''>
-          <div className='mb-5'>
-            <h1>Velkommen til Omvising</h1>
+          <div className='my-8'>
+            <h1 className="font-bold text-xl">Velkommen til Omvising</h1>
           </div>
-          <p></p>
+          <p>
+            Her bestiller du en avtale med oss 
+          </p>
           
-          <Link className='bg-orange-300 rounded-xl text-black text-bold px-4' href='/booking_tid'>Booking</Link>
+          <div className="px-16">
+            <Link className='bg-orange-300 rounded-xl text-black text-bold px-4' href='/booking_tid'>Booking</Link>
+          </div>
         </div>
       </main>
   );
