24455efcefb320c1a763e8dd8b69533183b50a11
Shiro <kristoffer_jakobsen@hotmail.com>
2026-08-23 03:14:05 +0200

    booking logikken ferdig :D bare styling og lett side manupilering igjen

--- Kris/booking/app/api/booking/route.ts
+++ Kris/booking/app/api/booking/route.ts
@@ -3,9 +3,39 @@ import getGoogleCalendar from "@/utils/googleCalendar";
 export async function POST(request: Request) {
     const booking = await request.json();
 
-    const calender = getGoogleCalendar()
+    const calendar = getGoogleCalendar()
 
-    console.log('Booking motatt: ', booking)
+    const start = new Date(booking.valgtDato)
 
-    return Response.json({success: true})
+    const [hour, minute] = booking.valgtTid.split(':').map(Number);
+
+    start.setHours(hour, minute, 0, 0)
+
+    const end = new Date(start);
+    end.setMinutes(end.getMinutes() + 30)
+
+    const event = await calendar.events.insert({
+        calendarId: process.env.GOOGLE_CALENDAR_ID,
+        requestBody: {
+            summary: `Booking - ${booking.name}`,
+            description: `
+            Name: ${booking.name}
+            E-post: ${booking.email}
+            Mobil: ${booking.mobil}
+            Melding: ${booking.message}
+            `,
+
+            start: {
+                dateTime: start.toISOString(),
+                timeZone: 'Europe/Oslo'
+            },
+
+            end: {
+                dateTime: end.toISOString(),
+                timeZone: 'Europe/Oslo'
+            }
+        }
+    })
+
+    return Response.json({success: true, eventId: event.data.id})
 }
